7af1f085955521bf89c8d2cc1ada9bfef6ca9467
Sebastian Gibb <mail@sebastiangibb.de>
2019-07-18 17:00:15 +0200

    Add vapply1{c,d,l}; closes #3

--- NEWS.md
+++ NEWS.md
@@ -2,6 +2,7 @@
 
 ## MsCoreUtils 0.0.1
 
+- Add `vapply1c`, `vapply1d` and `vapply1l`.
 - Add `bin`.
 - Add `closest`, `common` and `ppm`.
 - Package creation <2019-07-16 Tue>
---
+++ R/vapply.R
@@ -0,0 +1,38 @@
+#' vapply wrappers
+#'
+#' These functions are short wrappers around typical `vapply` calls for easier
+#' development.
+#'
+#' @param X a vector (atomic or `list`).
+#' @param FUN the `function` to be applied to each element of `X`.
+#' @param ... optional arguments to `FUN`.
+#' @param USE.NAMES `logical`, should the return value be named.
+#'
+#' @author Sebastian Gibb
+#' @export
+#' @rdname vapply
+#' @return `vapply1c` returns a vector of `character`s of length `X`.
+#' @aliases vapply1c
+#' @export
+vapply1c <- function(X, FUN, ..., USE.NAMES=FALSE) {
+    vapply(X=X, FUN=FUN, FUN.VALUE=NA_character_, ..., USE.NAMES=USE.NAMES)
+}
+
+#' @return `vapply1d` returns a vector of `double`s of length `X`.
+#' @rdname vapply
+#' @aliases vapply1d
+#' @export
+#' @examples
+#' l <- list(a=1:3, b=4:6)
+#' vapply1d(l, sum)
+vapply1d <- function(X, FUN, ..., USE.NAMES=FALSE) {
+    vapply(X=X, FUN=FUN, FUN.VALUE=NA_real_, ..., USE.NAMES=USE.NAMES)
+}
+
+#' @rdname vapply
+#' @return `vapply1l` returns a vector of `logical`s of length `X`.
+#' @aliases vapply1l
+#' @export
+vapply1l <- function(X, FUN, ..., USE.NAMES=FALSE) {
+    vapply(X=X, FUN=FUN, FUN.VALUE=NA, ..., USE.NAMES=USE.NAMES)
+}
