7b33733d07148b7244c32d6f15ae7ecc8a326c8c
Sebastian Gibb <mail@sebastiangibb.de>
2019-07-22 21:24:07 +0200

    Add valleys

---
+++ R/valleys.R
@@ -0,0 +1,48 @@
+#' Find Peak Valleys
+#'
+#' This function finds the valleys around peaks.
+#'
+#' @param x `numeric`, e.g. intensity values.
+#'
+#' @param p `numeric`, indices of identified peaks/local maxima.
+#'
+#' @return A `matrix` with three columns representing the index of the left
+#' valley, the peak center, and the right valley.
+#'
+#' @author Sebastian Gibb
+#' @export
+#' @examples
+#' ints <- c(5, 8, 12, 7, 4, 9, 15, 16, 11, 8, 3, 2, 3, 2, 9, 12, 14, 13, 8, 3)
+#' mzs <- seq_along(ints)
+#' peaks <- which(localMaxima(ints, hws = 3))
+#' cols <- seq_len(peaks) + 1
+#'
+#' plot(mzs, ints, type = "h", ylim = c(0, 16))
+#' points(mzs[peaks], ints[peaks], col = cols, pch = 20)
+#'
+#' v <- valleys(ints, peaks)
+#' segments(mzs[v[, "left"]], 0, mzs[v[, "right"]], col = cols, lwd = 2)
+valleys <- function(x, p) {
+    if (!is.numeric(x))
+        stop("'x' has to be numeric vector.")
+    if (!is.numeric(p))
+        stop("'p' has to be numeric vector.")
+
+    ## extend x on both sides to ensure that local minima at both extremes are
+    ## found
+    x <- c(Inf, x, Inf)
+    p <- p + 1L
+
+    v <- which(localMaxima(-x, hws = 1L))
+    ## local minima on the left of (before) the peaks
+    l <- v[findInterval(p, v)]
+    ## local minima on the right of (after) the peaks
+    ## could use rev here but that would cause dispatching and length checking
+    rv <- v[length(v):1L]
+    np <- length(p)
+    r <- rv[findInterval(-p[np:1L], -rv)[np:1L]]
+
+    m <- matrix(c(l, p, r), ncol = 3L,
+                dimnames = list(c(), c("left", "center", "right")))
+    m - 1L
+}
---
+++ tests/testthat/test_valleys.R
@@ -0,0 +1,13 @@
+test_that("valleys", {
+    expect_error(valleys(TRUE, 1), "numeric")
+    expect_error(valleys(1:10, TRUE), "numeric")
+
+    expect_equal(valleys(c(1:5, 4:1), 5),
+                 matrix(c(1, 5, 9), nrow = 1,
+                        dimnames = list(c(), c("left", "center", "right"))))
+    ## known bug/problem with plateaus c(0, 0) because localMaxima returns just
+    ## the first occurence of a local maximum
+    expect_equal(valleys(c(1:5, 4:1, 0, 0:3, 2), c(5, 14)),
+                 matrix(c(1, 5, 10, 10, 14, 15), nrow = 2, byrow = TRUE,
+                        dimnames = list(c(), c("left", "center", "right"))))
+})
