717314496980ba42e18a7c1a0ad03c9e196dda19
jorainer <johannes.rainer@gmail.com>
2019-09-02 13:10:07 +0200

    Add i2index utility function
    - Add the i2index function (`.i_to_index` in `Spectra` and `Chromatograms` for
      checking and converting subsetting parameter `i` to an integer index.
    - Add related documentation and unit tests.

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -5,3 +5,18 @@ test_that(".validateWindow", {
     expect_error(.validateWindow(11L, 10L), "smaller")
     expect_true(.validateWindow(3L, 10L))
 })
+
+test_that("i2index works", {
+    res <- i2index(3:4, 10)
+    expect_equal(res, 3:4)
+    expect_equal(i2index(-1, 10), -1)
+    expect_error(i2index(4, 3), "has to be between 1 and 3")
+
+    expect_error(i2index("a", 5), "object does not have names")
+    expect_error(i2index(c("a", "d"), 3, names = letters[1:3]), "not all names")
+    expect_equal(i2index(c("a", "d"), 4, names = letters[1:4]), c(1, 4))
+
+    expect_error(i2index(c(TRUE, FALSE), 3), "has to match the length")
+    expect_equal(i2index(rep(FALSE, 3), 3), integer())
+    expect_equal(i2index(c(FALSE, TRUE, TRUE), 3), 2:3)
+})
