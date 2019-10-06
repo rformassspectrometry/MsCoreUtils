c214dba9be7388d361971fa313c4b38c6a0e82d2
tnaake <thomasnaake@gmx.de>
2019-10-06 20:07:42 +0200

    test: add regexpr for expect_warning and expect_error

--- tests/testthat/test_dotproduct.R
+++ tests/testthat/test_dotproduct.R
@@ -8,8 +8,8 @@ test_that("dotproduct", {
     colnames(x) <- colnames(y) <- c("mz", "intensity")
     
     ## calculate similarity values
-    expect_warning(dotproduct(x, y, m = 0, n = 2))
-    expect_warning(dotproduct(x, y, n = 2))
+    expect_warning(dotproduct(x, y, m = 0, n = 2), "not identical")
+    expect_warning(dotproduct(x, y, n = 2), "not identical")
     expect_equal(dotproduct(x, y, m = 0.5, n = 0), 0.4280249, tolerance = 1e-06)
     expect_equal(dotproduct(x, y, m = 0, n = 0), 1)
     expect_equal(dotproduct(x, y, n = 0), 0.4280249, tolerance = 1e-06)
@@ -20,52 +20,56 @@ test_that("dotproduct", {
     expect_equal(dotproduct(y, y, n = 2), 1)
     
     ## check exceptions
-    expect_error(dotproduct(x[-1, ], y))
-    expect_error(dotproduct(x, y[-1, ]))
-    expect_error(dotproduct(x = x))
-    expect_error(dotproduct(y = y))
-    expect_error(dotproduct(x = x, y = "a"))
-    expect_error(dotproduct(x = "a", y = y))
-    expect_error(dotproduct(x, y, m = "a", n = 0))
-    expect_error(dotproduct(x, y, m = 0.5, n = "a"))
-    expect_error(dotproduct(x, y, m = 1:2, n = 0))
-    expect_error(dotproduct(x, y, m = 0.5, n = 1:2))
+    expect_error(dotproduct(x[-1, ], y), "nrow[(]x[)] and nrow[(]y[)]")
+    expect_error(dotproduct(x, y[-1, ]), "nrow[(]x[)] and nrow[(]y[)]")
+    expect_error(dotproduct(x = x), "is missing, with no default")
+    expect_error(dotproduct(y = y), "is missing, with no default")
+    expect_error(dotproduct(x = x, y = "a"), "not a matrix")
+    expect_error(dotproduct(x = "a", y = y), "not a matrix")
+    expect_error(dotproduct(x, y, m = "a", n = 0), 
+                 "has to be a numeric of length 1")
+    expect_error(dotproduct(x, y, m = 0.5, n = "a"), 
+                 "has to be a numeric of length 1")
+    expect_error(dotproduct(x, y, m = 1:2, n = 0),
+                 "has to be a numeric of length 1")
+    expect_error(dotproduct(x, y, m = 0.5, n = 1:2),
+                 "has to be a numeric of length 1")
     
     ## check for columns 'mz' and 'intensity'
     x <- matrix(c(1:3, 1:3), ncol = 2)
     y <- matrix(c(1:3, 1:3), ncol = 2)
     colnames(x) <- c("foo", "intensity")
     colnames(y) <- c("mz", "intensity")
-    expect_error(dotproduct(x, y))
+    expect_error(dotproduct(x, y), "subscript out of bounds")
     colnames(x) <- c("mz", "foo")
     colnames(y) <- c("mz", "intensity")
-    expect_error(dotproduct(x, y))
+    expect_error(dotproduct(x, y), "subscript out of bounds")
     colnames(x) <- c("mz", "intensity")
     colnames(y) <- c("foo", "intensity")
-    expect_error(dotproduct(x, y))
+    expect_error(dotproduct(x, y), "subscript out of bounds")
     colnames(x) <- c("mz", "intensity")
     colnames(y) <- c("mz", "foo")
-    expect_error(dotproduct(x, y))
+    expect_error(dotproduct(x, y), "subscript out of bounds")
     
     ## test for matrix
     x <- list(mz = 1:3, intensity = 1:3)
     y <- matrix(c(1:3, 1:3), ncol = 2)
     colnames(y) <- c("mz", "intensity")
-    expect_error(dotproduct(x, y))
+    expect_error(dotproduct(x, y), "is not a matrix")
     
     x <- matrix(c(1:3, 1:3), ncol = 2)
     colnames(x) <- c("mz", "intensity")
     y <- list(mz = 1:3, intensity = 1:3)
-    expect_error(dotproduct(x, y))
+    expect_error(dotproduct(x, y), "is not a matrix")
     
     ## test for mode 'numeric'
     x <- matrix(c(c("a", "b", "c"), c(1:3)), ncol = 2)
     y <- matrix(c(1:3, 1:3), ncol = 2)
     colnames(x) <- colnames(y) <- c("mz", "intensity")
-    expect_error(dotproduct(x, y))
+    expect_error(dotproduct(x, y), "non-numeric argument to binary operator")
     x <- matrix(c(1:3, 1:3), ncol = 2) 
     y <- matrix(c(c("a", "b", "c"), c(1:3)), ncol = 2)
     colnames(x) <- colnames(y) <- c("mz", "intensity")
-    expect_error(dotproduct(x, y))
+    expect_error(dotproduct(x, y), "non-numeric argument to binary operator")
     
 })
