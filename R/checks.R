a9be6c84d739fbd59bae4bb97e50acf2c5bd7b83
Sebastian Gibb <mail@sebastiangibb.de>
2019-10-12 21:37:28 +0200

    feat: add {is,valid}PeaksMatrix

---
+++ R/checks.R
@@ -0,0 +1,23 @@
+#' @title Check functions
+#'
+#' @description
+#' These functions are used to check input arguments.
+#'
+#' @param x object to test.
+#'
+#' @return `logical(1)`, `TRUE` if checks are successful otherwise `FALSE`.
+#'
+#' @author Sebastian Gibb
+#' @family helper functions for developers
+#' @export
+#' @examples
+#' isPeaksMatrix(1:2)
+#' isPeaksMatrix(cbind(mz = 1:2, intensity = 1:2))
+isPeaksMatrix <- function(x) {
+    isTRUE(
+        is.matrix(x) &&
+        mode(x) == "numeric" &&
+        ncol(x) == 2L &&
+        colnames(x) == c("mz", "intensity")
+    )
+}
---
+++ R/validation.R
@@ -0,0 +1,22 @@
+#' @title Validation functions
+#'
+#' @description
+#' These functions are used to validate input arguments.
+#'
+#' @param x object to test.
+#'
+#' @return `logical(1)`, `TRUE` if validation are successful otherwise an error
+#' is thrown.
+#'
+#' @author Sebastian Gibb
+#' @family helper functions for developers
+#' @export
+#' @examples
+#' try(validPeaksMatrix(1:2))
+#' validPeaksMatrix(cbind(mz = 1:2, intensity = 1:2))
+validPeaksMatrix <- function(x) {
+    if (!isPeaksMatrix(x))
+        stop("'x' has to be a 'numeric' matrix with two columns named ",
+             "'mz' and 'intensity'.")
+    TRUE
+}
