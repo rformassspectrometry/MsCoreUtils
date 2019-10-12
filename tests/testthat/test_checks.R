7470fdf2bc40b94623a7126b6f86fdc8ba0f0374
Sebastian Gibb <mail@sebastiangibb.de>
2019-10-12 22:09:32 +0200

    feat: check for missing colnmaes

--- tests/testthat/test_checks.R
+++ tests/testthat/test_checks.R
@@ -3,6 +3,7 @@ test_that("isPeaksMatrix", {
     expect_false(isPeaksMatrix(matrix(1:3, nrow = 1)))
     expect_false(isPeaksMatrix(matrix(1:2, ncol = 2)))
     expect_false(isPeaksMatrix(cbind(foo = 1, bar = 2)))
+    expect_false(isPeaksMatrix(cbind(mz = 1, bar = 2)))
     expect_false(isPeaksMatrix(cbind(mz = "A", intensity = "B")))
     expect_true(isPeaksMatrix(cbind(mz = 1:2, intensity = 1:2)))
 })
