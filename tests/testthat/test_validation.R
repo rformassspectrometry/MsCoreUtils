42edd405236881d5ab824ec3cb985559836deb41
Sebastian Gibb <mail@sebastiangibb.de>
2019-10-14 13:18:47 +0200

    feat: check for unsorted mz column as suggested by @jorainer

--- tests/testthat/test_validation.R
+++ tests/testthat/test_validation.R
@@ -4,5 +4,6 @@ test_that("validPeaksMatrix", {
     expect_error(validPeaksMatrix(matrix(1:2, ncol = 2)), "'mz' and 'intensity'")
     expect_error(validPeaksMatrix(cbind(foo = 1, bar = 2)), "'mz' and 'intensity'")
     expect_error(validPeaksMatrix(cbind(mz = "A", intensity = "B")), "'numeric'")
+    expect_error(validPeaksMatrix(cbind(mz = 2:1, intensity = 2:1)), "sorted")
     expect_true(validPeaksMatrix(cbind(mz = 1:2, intensity = 1:2)))
 })
