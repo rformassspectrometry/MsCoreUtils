bd497fb0bb5382313e0d1b300eab303a9f1106d0
Sebastian Gibb <mail@sebastiangibb.de>
2019-12-11 12:03:20 +0100

    feat: add .weightxy

--- R/distance.R
+++ R/distance.R
@@ -11,3 +11,26 @@
 .calibrate <- function(x, offset = 0L, scaling = 1L) {
     (x - offset) / scaling
 }
+
+#' Weighted X Y function
+#'
+#' @param x `numeric`, e.g. mz values.
+#' @param y `numeric`, e.g. intensity values.
+#' @param m `numeric`, weighting `x`.
+#' @param n `numeric`, weighting `y`.
+#'
+#' @return `numeric`.
+#'
+#' @references
+#' Stein, S. E., and Scott, D. R. (1994).
+#' Optimization and testing of mass spectral library search algorithms for
+#' compound identification.
+#' Journal of the American Society for Mass Spectrometry, 5(9), 859-866.
+#' \doi{10.1016/1044-0305(94)87009-8}.
+#'
+#' @noRd
+#' @examples
+#' .weightxy(1:3, 4:6)
+.weightxy <- function(x, y, m = 0, n = 0.5) {
+    x ^ m * y ^ n
+}
--- tests/testthat/test_distance.R
+++ tests/testthat/test_distance.R
@@ -3,3 +3,8 @@ test_that(".calibrate", {
     expect_equal(.calibrate(1:3, 2, 2), (-1:1)/2)
     expect_equal(.calibrate(1:3, scaling = 0), rep(Inf, 3))
 })
+
+test_that(".weightxy", {
+    expect_equal(.weightxy(1:3, 4:6), sqrt(4:6))
+    expect_equal(.weightxy(1:3, 4:6, m = -1, n = 2), (4:6)*(4:6)/1:3)
+})
