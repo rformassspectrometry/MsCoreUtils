c947cdab5728f06e0ec1446d54fb0dc3656de173
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-02-04 12:34:22 +0100

    feat: add new and clarify normalisations

--- R/normalize.R
+++ R/normalize.R
@@ -1,10 +1,13 @@
 ##' @export
-##' 
+##'
 ##' @rdname normalize
 normalizeMethods <- function()
-    c("sum", "max", "center.mean",
-      "center.median", "diff.median",
-      "quantiles", "quantiles.robust", "vsn")
+    c("sum", "max",
+      "center.mean", "center.median",
+      "div.mean", "div.median",
+      "diff.median",
+      "quantiles", "quantiles.robust",
+      "vsn")
 
 
 ##' @title Quantitative data normalisation
@@ -17,44 +20,55 @@ normalizeMethods <- function()
 ##'
 ##' @details
 ##'
-##' The `method` parameter in `normalize` can be one of `"sum"`,
-##' `"max"`, `"quantiles"`, `"center.mean"`, `"center.median"`,
-##' `"center.median"`, `"quantiles.robust`" or `"vsn"`.  For `"sum"`
-##' and `"max"`, each feature's intensity is divided by the maximum or
-##' the sum of the feature respectively. These two methods are applied
-##' along the features (rows). The `normalizeMethods()` function
-##' returns a vector of available normalisation methods.
+##' The `method` parameter can be one of `"sum"`, `"max"`, `"center.mean"`,
+##' `"center.median"`, `"div.mean"`, `"div.median"`, `"diff.meda"`,
+##' `"quantiles`", `"quantiles.robust`" or `"vsn"`. The `normalizeMethods()`
+##' function returns a vector of available normalisation methods.
 ##'
-##' `"center.mean"` and `"center.median"` translate the respective
-##' sample (column) intensities according to the column mean or
-##' median. `"diff.median"` translates all samples (columns) so that
-##' they all match the grand median. Using `"quantiles"` or
-##' `"quantiles.robust"` applies (robust) quantile normalisation, as
-##' implemented in [preprocessCore::normalize.quantiles()] and
-##' [preprocessCore::normalize.quantiles.robust()]. `"vsn"` uses the
-##' [vsn::vsn2()] function.  Note that the latter also glog-transforms
-##' the intensities.  See respective manuals for more details and
-##' function arguments.
-##' 
+##' - For `"sum"` and `"max"`, each feature's intensity is divided by the
+##'   maximum or the sum of the feature respectively. These two methods are
+##'   applied along the features (rows).
+##'
+##' - `"center.mean"` and `"center.median"` center the respective sample
+##'   (column) intensities by subtracting the respective column means or
+##'   medians. `"div.mean"` and `"div.median"` divide by the column means or
+##'   medians.
+##'
+##' - `"diff.median"` centers all samples (columns) so that they all match the
+##'   grand median by subtracting the respective columns medians differences to
+##'   the grand median.
+##'
+##' - Using `"quantiles"` or `"quantiles.robust"` applies (robust) quantile
+##'   normalisation, as implemented in [preprocessCore::normalize.quantiles()]
+##'   and [preprocessCore::normalize.quantiles.robust()]. `"vsn"` uses the
+##'   [vsn::vsn2()] function.  Note that the latter also glog-transforms the
+##'   intensities.  See respective manuals for more details and function
+##'   arguments.
+##'
+##' @seealso The [scale()] function that centers (like `center.mean` above) and
+##'     scales.
+##'
 ##' @param x A matrix to be normalised.
 ##'
 ##' @param method `character(1)` defining the normalisation
 ##'     method. See `normalizeMethods()` for available ones.
-##' 
+##'
 ##' @param ... Additional parameters passed to the inner normalisation
 ##'     function.
-##' 
+##'
 ##' @return A normalised matrix of dimensions `dim(x)`.
 ##'
 ##' @export
 ##'
 ##' @importFrom stats median
-##' 
+##'
 ##' @rdname normalize
-##' 
+##'
 ##' @author Laurent Gatto
 ##'
 ##' @examples
+##' normalizeMethods()
+##'
 ##' ## test data
 ##' set.seed(42)
 ##' m <- matrix(rlnorm(60), 10)
@@ -67,6 +81,13 @@ normalizeMethods <- function()
 ##'
 ##' normalize_matrix(m, method = "center.mean")
 normalize_matrix <- function(x, method, ...) {
+    if (missing(method))
+        stop("Please specify a normalization method. ",
+             "See '?normalize_matrix' for details.")
+    method <- match.arg(method,
+                        choices = normalizeMethods(),
+                        several.ok = FALSE)
+
     if (method == "vsn") {
         requireNamespace("vsn")
         e <- vsn::vsn2(x, ...)@hx
@@ -78,15 +99,21 @@ normalize_matrix <- function(x, method, ...) {
         e <- preprocessCore::normalize.quantiles.robust(x, ...)
     } else if (method == "center.mean") {
         center <- colMeans(x, na.rm = TRUE)
-        e <- sweep(x, 2L, center, check.margin = FALSE, ...)
+        e <- sweep(x, 2L, center, FUN = "-", check.margin = FALSE, ...)
     } else if (method == "center.median") {
         center <- apply(x, 2L, median, na.rm = TRUE)
-        e <- sweep(x, 2L, center, check.margin = FALSE, ...)
+        e <- sweep(x, 2L, center, FUN = "-", check.margin = FALSE, ...)
+    } else if (method == "div.mean") {
+        center <- colMeans(x, na.rm = TRUE)
+        e <- sweep(x, 2L, center, FUN = "/", check.margin = FALSE, ...)
+    } else if (method == "div.median") {
+        center <- apply(x, 2L, median, na.rm = TRUE)
+        e <- sweep(x, 2L, center, FUN = "/", check.margin = FALSE, ...)
     } else if (method == "diff.median") {
         med <- median(as.numeric(x), na.rm = TRUE)
         cmeds <- apply(x, 2L, median, na.rm = TRUE)
-        e <- sweep(x, 2L, cmeds - med)
-    } else {
+        e <- sweep(x, 2L, cmeds - med, FUN = "-")
+    } else { ## max or sum
         switch(method,
                max = div <- apply(x, 1, max, na.rm = TRUE),
                sum = div <- rowSums(x, na.rm = TRUE))
