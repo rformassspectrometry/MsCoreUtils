cb2be9ffb2014e7cfcc95aecffc5498d7feb71c6
Sebastian Gibb <mail@sebastiangibb.de>
2020-05-08 14:29:04 +0200

    feat: add ndotproduct

--- R/distance.R
+++ R/distance.R
@@ -1,3 +1,79 @@
+#' @name distance
+#' @title Spectra Distance/Similarity Measurements
+#'
+#' @description
+#' These functions provide different similariy/distance measurements.
+#'
+#' @param x `matrix`, two-columns e.g. m/z, intensity
+#' @param y `matrix`, two-columns e.g. m/z, intensity
+#'
+#' @details
+#'
+#' @return `double(1)` value between `0:1`, where `0` is completely different
+#' and `1` identically.
+#'
+#' @rdname distance
+#' @author Sebastian Gibb
+#' @family distance/similarity functions
+#' @references
+#' Stein, S. E., and Scott, D. R. (1994).
+#' Optimization and testing of mass spectral library search algorithms for
+#' compound identification.
+#' Journal of the American Society for Mass Spectrometry, 5(9), 859--866.
+#' \doi{10.1016/1044-0305(94)87009-8}.
+#'
+#' Horai et al. (2010).
+#' MassBank: a public repository for sharing mass spectral data for life
+#' sciences. Journal of mass spectrometry, 45(7), 703-714.
+#' \doi{https://doi.org/10.1002/jms.1777}.
+#' @examples
+#'
+#' x <- matrix(c(1:5, 1:5), ncol = 2, dimnames = list(c(), c("mz", "intensity")))
+#' y <- matrix(c(1:5, 5:1), ncol = 2, dimnames = list(c(), c("mz", "intensity")))
+NULL
+
+#' @rdname distance
+#'
+#' @param m `numeric`, weighting for the first column of `x` and `y` (e.g.
+#' "mz"), default: `0` means don't weight by the first column.
+#' @param n `numeric`, weighting for the second column of `x` and `y` (e.g.
+#' "intensity), default: `0.5` means effectly using `sqrt(x[,2])` and
+#' `sqrt(y[,2])`
+#' @param na.rm `logical(1)`, should `NA` removed prior to calculation (default
+#' `TRUE`).
+#'
+#' @details
+#' `ndotproduct`: the normalized dot product is described in Stein and Scott
+#' 1994 as: \eqn{NDP = \frac{\sum(W_1 W_2)^2}{\sum(W_1)^2 \sum(W_2)^2}}; where
+#' \eqn{W_i = x^m * y^n}, where \eqn{x} and \eqn{y} are the mz and intensity values,
+#' respectively. Stein and Scott 1994 empirically determined the optimal
+#' exponents as `m = 3` and `n = 0.6` by analyzing ca. 12000 EI-MS data of
+#' 8000 organic compounds in the NIST Mass Spectral Library.
+#' MassBank (Horai et al. 2010) uses `m = 2` and `n = 0.5`
+#' for small compounds. In general with increasing values for `m`,
+#' high m/z values will be taken more into account for similarity calculation.
+#' Especially when working with small molecules, a value `n > 0` can be set
+#' to give a weight on the m/z values to accommodate that shared fragments
+#' with higher m/z are less likely and will mean that molecules might be more
+#' similar. Increasing `n` will result in a higher importance of the intensity
+#' values. Most commonly `m = 0` and `n = 0.5` are used.
+#'
+#' @author
+#' `ndotproduct`: Sebastian Gibb and
+#' Thomas Naake, \email{thomasnaake@@googlemail.com}
+#'
+#' @export
+#' @aliases ndotproduct
+#' @examples
+#'
+#' ndotproduct(x, y)
+ndotproduct <- function(x, y, m = 0L, n = 0.5, na.rm = TRUE) {
+    wx <- .weightxy(x[, 1L], x[, 2L], m, n)
+    wy <- .weightxy(y[, 1L], y[, 2L], m, n)
+    sum(wx * wy, na.rm = na.rm)^2L /
+        (sum(wx^2L, na.rm = na.rm) * sum(wy^2L, na.rm = na.rm))
+}
+
 #' Calibrate function (workhorse of normalise)
 #'
 #' @param x `numeric`.
--- tests/testthat/test_distance.R
+++ tests/testthat/test_distance.R
@@ -1,3 +1,12 @@
+x <- matrix(c(1:5, 1:5), ncol = 2)
+y <- matrix(c(1:5, 5:1), ncol = 2)
+
+test_that("ndotproduct", {
+    expect_equal(ndotproduct(x, x), 1)
+    expect_equal(ndotproduct(x, y), 0.7661, tolerance = 1e-5)
+    expect_equal(ndotproduct(x, y, m = 3, n = 0.6), 0.91276, tolerance = 1e-5)
+})
+
 test_that(".calibrate", {
     expect_equal(.calibrate(1:3), 1:3)
     expect_equal(.calibrate(1:3, 2, 2), (-1:1)/2)
