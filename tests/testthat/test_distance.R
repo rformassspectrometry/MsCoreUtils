144b34eb5954149632f4b31975b65c48e460706d
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-05-22 20:03:30 +0200

    test: add dotproduct deprecation test

--- tests/testthat/test_distance.R
+++ tests/testthat/test_distance.R
@@ -35,3 +35,9 @@ test_that(".weightxy", {
     expect_equal(.weightxy(1:3, 4:6), sqrt(4:6))
     expect_equal(.weightxy(1:3, 4:6, m = -1, n = 2), (4:6)*(4:6)/1:3)
 })
+
+test_that("dotproduct", {
+    expect_warning(res1 <- dotproduct(x, y))
+    expect_equal(res1, ndotproduct(x, y))
+
+})
