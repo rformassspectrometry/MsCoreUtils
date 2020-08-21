4d313ff68edf33e38568eb6c45c3c6d5eb129acf
jorainer <johannes.rainer@gmail.com>
2020-08-21 14:18:04 +0200

    refactor: re-add lines deleted during merge

--- src/MsCoreUtils.h
+++ src/MsCoreUtils.h
@@ -16,6 +16,8 @@ extern SEXP C_join_outer(SEXP, SEXP, SEXP);
 
 extern SEXP C_join_left(SEXP, SEXP, SEXP);
 
+extern SEXP C_localMaxima(SEXP, SEXP);
+
 extern SEXP _MsCoreUtils_imp_neighbour_avg(SEXP, SEXP);
 
 #endif /* end of MSCOREUTILS_H */
