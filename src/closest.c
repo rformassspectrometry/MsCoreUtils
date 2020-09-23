f28a64f2aaece9e10f3504c43637e5864136e77a
Sebastian Gibb <mail@sebastiangibb.de>
2020-09-23 13:37:58 +0200

    refactor: simplify smallest difference decision statements

--- src/closest.c
+++ src/closest.c
@@ -68,13 +68,14 @@ SEXP C_closest_dup_keep(SEXP x, SEXP table, SEXP tolerance, SEXP nomatch) {
  * \return index the closest element
  *
  * \note x and table have to be sorted increasingly and not containing any NA.
+ * \author Sebastian Gibb and Johannes Rainer
  */
 SEXP C_closest_dup_closest(SEXP x, SEXP table, SEXP tolerance, SEXP nomatch) {
     double *px = REAL(x);
     const unsigned int nx = LENGTH(x);
 
     double *ptable = REAL(table);
-    const unsigned int ntable1 = LENGTH(table) - 1;
+    const unsigned int ntable = LENGTH(table);
 
     double *ptolerance = REAL(tolerance);
 
@@ -82,61 +83,45 @@ SEXP C_closest_dup_closest(SEXP x, SEXP table, SEXP tolerance, SEXP nomatch) {
     int* pout = INTEGER(out);
 
     const unsigned int inomatch = asInteger(nomatch);
-    unsigned int j = 1, lastj = 0;
-    double prevdiff = R_PosInf, nextdiff = R_PosInf, lastdiff = R_PosInf;
 
-    for (int i = 0; i < nx; ++i) {
-        while(j < ntable1 && ptable[j] < px[i])
-            ++j;
+    unsigned int ix = 0, ixlastused = 1;
+    unsigned int itbl = 0, itbllastused = 1;
+    double diff = R_PosInf, diffnxtx = R_PosInf, diffnxttbl = R_PosInf;
 
-        /* fabs should be just needed for the first element */
-        prevdiff = fabs(px[i] - ptable[j - 1]);
-        nextdiff = fabs(ptable[j] - px[i]);
+    while (ix < nx) {
+        if (itbl < ntable) {
+            /* difference for current pair */
+            diff = fabs(px[ix] - ptable[itbl]);
+            /* difference for next pairs */
+            diffnxtx =
+                ix + 1 < nx ? fabs(px[ix + 1] - ptable[itbl]) : R_PosInf;
+            diffnxttbl =
+                itbl + 1 < ntable ? fabs(px[ix] - ptable[itbl + 1]) : R_PosInf;
 
-        if (prevdiff <= ptolerance[i] || nextdiff <= ptolerance[i]) {
-            if (prevdiff < nextdiff) {
-                /* match on the left */
-                if (lastj == j) {
-                    if (prevdiff < lastdiff) {
-                        /* same match as before but with smaller difference */
-                        pout[i] = j;
-                        pout[i - 1] = inomatch;
-                        lastdiff = prevdiff;
-                    } else
-                        pout[i] = inomatch;
-                } else {
-                    pout[i] = j;
-                    lastdiff = prevdiff;
-                }
-            } else if (prevdiff > nextdiff) {
-                /* match on the right */
-                if (lastj == j + 1) {
-                    if (nextdiff < lastdiff) {
-                        /* same match as before but with smaller difference */
-                        pout[i] = ++j;
-                        pout[i - 1] = inomatch;
-                        lastdiff = nextdiff;
-                    } else
-                        pout[i] = inomatch;
-                } else {
-                    pout[i] = ++j;
-                    lastdiff = nextdiff;
-                }
+            if (diff <= ptolerance[ix]) {
+                /* valid match, add + 1 to convert between R/C index */
+                pout[ix] = itbl + 1;
+                if (itbl == itbllastused &&
+                        (diffnxtx < diffnxttbl || diff < diffnxttbl))
+                    pout[ixlastused] = inomatch;
+                ixlastused = ix;
+                itbllastused = itbl;
+            } else
+                pout[ix] = inomatch;
+
+            if (diffnxtx < diff || diffnxttbl < diff) {
+                /* increment the index with the smaller distance */
+                if (diffnxtx < diffnxttbl)
+                    ++ix;
+                else
+                    ++itbl;
             } else {
-                /* match on the left or right */
-                if (lastj == j && prevdiff > lastdiff) {
-                    pout[i] = ++j;
-                    lastdiff = nextdiff;
-                } else {
-                    pout[i] = j;
-                    lastdiff = prevdiff;
-                }
+                /* neither next x nor next table item offer a better match */
+                ++ix;
+                ++itbl;
             }
-            lastj = j;
-        } else {
-            pout[i] = inomatch;
-            lastdiff = R_PosInf;
-        }
+        } else
+            pout[ix++] = inomatch;
     }
 
     UNPROTECT(1);
