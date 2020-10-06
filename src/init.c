3317fc14194b05e7451bf8143971e3c750de3d72
Sebastian Gibb <mail@sebastiangibb.de>
2020-10-06 12:10:56 +0200

    refactor: remove old outer/left join C implementation

--- src/init.c
+++ src/init.c
@@ -5,11 +5,9 @@ static const R_CallMethodDef CallEntries[] = {
     {"C_closest_dup_closest", (DL_FUNC) &C_closest_dup_closest, 4},
     {"C_closest_dup_remove", (DL_FUNC) &C_closest_dup_remove, 4},
     {"C_impNeighbourAvg", (DL_FUNC) &C_impNeighbourAvg, 2},
-    {"C_join_outer", (DL_FUNC) &C_join_outer, 3},
-    {"C_join_outer2", (DL_FUNC) &C_join_outer2, 4},
-    {"C_join_left", (DL_FUNC) &C_join_left, 3},
-    {"C_join_left2", (DL_FUNC) &C_join_left2, 4},
-    {"C_join_inner2", (DL_FUNC) &C_join_inner2, 4},
+    {"C_join_outer", (DL_FUNC) &C_join_outer, 4},
+    {"C_join_left", (DL_FUNC) &C_join_left, 4},
+    {"C_join_inner", (DL_FUNC) &C_join_inner, 4},
     {"C_localMaxima", (DL_FUNC) &C_localMaxima, 2},
     {NULL, NULL, 0}
 };
