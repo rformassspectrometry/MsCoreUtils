c472b4aa5bbe7af44db09f2d4faec730a43345d2
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-10-29 11:43:34 +0100

    tests: test vectorsied foramtRt() and empty inputs

--- tests/testthat/test_formatRt.R
+++ tests/testthat/test_formatRt.R
@@ -1,7 +1,7 @@
 test_that("formatRt", {
     tc <- c("1:01", "25:24")
     tn <- c(61, 25 * 60 + 24)
-    expect_equal(tc, formatRt(tn))    
+    expect_equal(tc, formatRt(tn))
     expect_equal(tn, formatRt(tc))
     expect_true(is.na(formatRt("")))
     expect_warning(is.na(formatRt("aaa")))
@@ -9,4 +9,12 @@ test_that("formatRt", {
     expect_equal(formatRt(1), "0:01")
     expect_equal(formatRt(60), "1:00")
     expect_equal(formatRt(61), "1:01")
+    expect_error(formatRt(numeric()))
+    expect_error(formatRt(character()))
+    expect_error(formatRt(list()))
+    expect_identical(c("0:01", "0:02", "0:03", "0:04", "0:05", "0:06",
+                       "0:07", "0:08", "0:09", "0:10"),
+                     formatRt(1:10))
+    expect_equal(formatRt(c("25:24", "25:25", "25:26")),
+                 c(1524:1526))    
 })
