9a5e05b56fdfce6db4efa758845dad359b83c224
jorainer <johannes.rainer@gmail.com>
2022-04-12 12:51:19 +0200

    refactor: function bin gains `returnMids` parameter
    - Add parameter `returnMids` to disable automatic return of bin midpoints in the
      `bin` function. Using `returnMids = FALSE` will have a considerable impact on
      performance if `bin` is used on multiple pairs of `x` and `y` values (e.g.
      multiple spectra) in a loop and when breaks can be determined beforehand.

--- R/binning.R
+++ R/binning.R
@@ -16,10 +16,22 @@
 #' @param breaks `numeric` defining the breaks (bins).
 #'
 #' @param FUN `function` to be used to aggregate values of `x` falling into the
-#'     bins defined by `breaks`.
+#'     bins defined by `breaks`. `FUN` is expected to return a `numeric(1)`.
 #'
-#' @return `list` with elements `x` (aggregated values of `x`) and `mids` (the
-#'     bin mid points).
+#' @param returnMids `logical(1)` whether the midpoints for the breaks should be
+#'     returned in addition to the binned (aggregated) values of `x`. Setting
+#'     `returnMids = FALSE` might be useful if the breaks are defined before
+#'     hand and binning needs to be performed on a large set of values (i.e.
+#'     within a loop for multiple pairs of `x` and `y` values).
+#'
+#' @return
+#'
+#' Depending on the value of `returnMids`:
+#'
+#' - `returnMids = TRUE` (the default): returns a `list` with elements `x`
+#'   (aggregated values of `x`) and `mids` (the bin mid points).
+#' - `returnMids = FALSE`: returns a `numeric` with just the binned values for
+#'   `x`.
 #'
 #' @author Johannes Rainer, Sebastian Gibb
 #'
@@ -38,9 +50,13 @@
 #'
 #' ## Repeat but summing up intensities instead of taking the max
 #' bin(ints, mz, size = 2, FUN = sum)
+#'
+#' ## Get only the binned values without the bin mid points.
+#' bin(ints, mz, size = 2, returnMids = FALSE)
 bin <- function(x, y, size = 1,
                 breaks = seq(floor(min(y)),
-                             ceiling(max(y)), by = size), FUN = max) {
+                             ceiling(max(y)), by = size), FUN = max,
+                returnMids = TRUE) {
     if (length(x) != length(y))
         stop("lengths of 'x' and 'y' have to match.")
     FUN <- match.fun(FUN)
@@ -52,9 +68,10 @@ bin <- function(x, y, size = 1,
     idx[idx >= nbrks] <- nbrks - 1L
 
     ints <- double(nbrks - 1L)
-    ints[unique(idx)] <- unlist(lapply(base::split(x, idx), FUN),
-                                use.names = FALSE)
-    list(x = ints, mids = (breaks[-nbrks] + breaks[-1L]) / 2L)
+    ints[unique(idx)] <- vapply(base::split(x, idx), FUN, numeric(1))
+    if (returnMids)
+        list(x = ints, mids = (breaks[-nbrks] + breaks[-1L]) / 2L)
+    else ints
 }
 
 #' Simple function to ensure that breaks (for binning) are spaning at least the
