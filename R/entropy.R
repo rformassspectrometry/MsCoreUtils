1a5b0539a993040dde1bf6ac8ddee2d20dbf1b1e
Mar Garcia Aloy <mar.garcia.aloy@gmail.com>
2023-05-23 15:40:26 +0200

    updates with suggestions of jorainer and lgatto

--- R/entropy.R
+++ R/entropy.R
@@ -2,11 +2,19 @@
 #'
 #' @description
 #'
-#' These functions allows to calculate entropy measurements of an MS/MS  
+#' These functions allow to calculate entropy measurements of an MS/MS  
 #' spectrum based on the metrics suggested by 
 #' Li et al. (https://doi.org/10.1038/s41592-021-01331-z).
+#' Spectral entropy and normalized entropy are used to measure the complexity 
+#' of an spectra.
+#' MassBank of North America (MoNA) defines spectra entropy as the intensity 
+#' weighted spectral peak number 
+#' (https://mona.fiehnlab.ucdavis.edu/documentation/entropy. 
+#' Additionally it is suggested to label as low-quality spectrums the ones 
+#' that have a normalized entropy higher than 0.8 and spectral entropy 
+#' higher than 3. 
 #'
-#' @param x `numeric`, intensities of the fragment ions value(s).
+#' @param x `numeric`, intensities of the fragment ions(s).
 #'
 #' @return `numeric`: (normalized) entropy `x`.
 #'
@@ -26,15 +34,14 @@
 #' @examples
 #' spectrum <- rbind(c(41.04, 37.16), c(69.07, 66.83), c(86.1, 999.0))
 #'
-#' entropy_spectral(spectrum[,2])
-#' entropy_normalized(spectrum[,2])
+#' entropy(spectrum[,2])
+#' nentropy(spectrum[,2])
 #' 
-entropy_spectral <- function(x){
+entropy <- function(x) {
   y <- x / sum(x)
-  z <- y*log(y)
-  return(-sum(z))
+  -sum(y * log(y))
 }
 
-entropy_normalized <- function(x){
-  entropy_spectral(x) / log(length(x))
+nentropy <- function(x) {
+  entropy(x) / log(length(x))
 }
