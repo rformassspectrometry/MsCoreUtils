a034d44d1c8afb01cc4135f08b895ace564b5aec
Johannes Rainer <johannes.rainer@gmail.com>
2026-04-07 14:06:56 +0200

    refactor: address Laurent's comments

--- R/retry.R
+++ R/retry.R
@@ -16,7 +16,7 @@
 #'
 #' @param retry_on `character(1)` pattern for the error message to retry `expr`.
 #'     Defaults to `retry_on = "*"` hence retrying `expr` on any error that
-#'     occurrs. This allows to restrict retrying `expr` for specific cases,
+#'     occurs. This allows to restrict retrying `expr` for specific cases,
 #'     such as temporary internet connection problems. The pattern defined by
 #'     `retry_on` is directly passed to the `grepl()` function.
 #'
@@ -31,6 +31,18 @@
 #' @importFrom methods is
 #'
 #' @export
+#'
+#' @examples
+#'
+#' ## In the example below content is read from a web page that might
+#' ## temporarily be offline. It is assumed that the error message contains
+#' ## the pattern `"temporarily"` in which case the call is repeated 7 times
+#' ## with an increasing interval between tries to allow the resource to
+#' ## become available again.
+#' \dontrun{
+#'     res <- retry(readLines("https://some-unreliable-web-content"),
+#'                  retry_on = "temporarily", ntimes = 7L, sleep_mult = 10)
+#' }
 retry <- function(expr, ntimes = 5L, sleep_mult = 0L,
                   retry_on = "*", ...) {
     res <- NULL
--- tests/testthat/test_retry.R
+++ tests/testthat/test_retry.R
@@ -5,11 +5,13 @@ test_that("retry works", {
         1
     }
     set.seed(123)
+    ## With a seed of 123 `a()` throws an error 3 times and works on the 4th
     res <- retry(a(), ntimes = 5L)
     expect_equal(res, 1)
-    ## Failure
+    ## Failure (fails 3 consecutive times)
     set.seed(123)
     expect_error(retry(a(), ntimes = 3L), "A, got a 0")
 
+    ## Always fails
     expect_error(retry(hello, ntimes = 5L, retry_on = "not found"), "not found")
 })
