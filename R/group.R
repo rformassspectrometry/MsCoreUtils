7d6f6a5463780314de0cd10777c672476357c100
Muyao Xi <rfm848@ku.dk>
2026-04-29 10:55:59 +0200

    modify based on recommendations

--- R/group.R
+++ R/group.R
@@ -100,7 +100,7 @@ group <- function(x, tolerance = 0, ppm = 0) {
 #'
 #' @return `integer` of length equal to `x` with the groups.
 #'
-#' @author Muyao Xi
+#' @author ctb (Muyao Xi)
 #'
 #' @rdname group_mz_int
 #'
@@ -130,70 +130,58 @@ group <- function(x, tolerance = 0, ppm = 0) {
 #' group_mz_int(x, y, max_num, tolerance = 0.005)
 #'
 #' ## the same three groups were made.
+#' 
+#' @importFrom utils tail
 
 
 
-group_mz_int <- function(x, y, max_num, tolerance = 0, ppm = 0) {
+group_mz_int <- function(x, y, max_num = Inf, tolerance = 0, ppm = 0) {
     #group m/z within tolerance based on the intensity decreasingly
-    # if(is.unsorted(x)) {
-    #     idx <- order(x)
-    #     x <- x[idx]
-    #     y <- y[idx]
-    # } else idx <- integer()
+    
     
     tolerance <- rep(tolerance + sqrt(.Machine$double.eps), length(x))
     if (ppm > 0)
-        tolerance <- tolerance + ppm(x[1:length(x)], ppm)
+        tolerance <- tolerance + ppm(x, ppm)
     
     
     
-    #df_xy <- data.frame("mz" = x, "int" = y, "group_id" = rep(0, length(x)), "index" = 1:length(x))
-    
+
     mz <- x
     int <- y
     group_ids <- rep(0, length(x))
     index <- 1:length(x)
     
-    #group_id_zero <- which(df_xy$group_id == 0)
     group_id_zero <- which(group_ids == 0)
     group_id <- 0
     
     
     while (length(group_id_zero) != 0) {
         group_id <- group_id + 1
-        #id_int_max <- group_id_zero[which.max(df_xy[group_id_zero, "int"])]
         id_int_max <- group_id_zero[which.max(int[group_id_zero])]
         
-        #logi_id <- with(df_xy, abs(df_xy[group_id_zero, "mz"]-df_xy[id_int_max, "mz"])<=tolerance[group_id_zero])
-        
+
         logi_id <- abs(mz[group_id_zero]-mz[id_int_max])<=tolerance[group_id_zero]
         
         if (sum(logi_id) == 0) {
-            #df_xy[id_int_max, "group_id"] <- group_id
             group_ids[id_int_max] <- group_id
             
-        } else if (sum(logi_id) > max_num) {
+        } else if (is.finite(max_num) && sum(logi_id) > max_num) {
             
-            #int_index <- df_xy[group_id_zero[logi_id], c("int", "index")]
-            
+
             int_logi <- int[group_id_zero[logi_id]]
             index_logi <- index[group_id_zero[logi_id]]
             
-            #index_sel_int <- order(int_index$int)
             index_sel_int <- order(int_logi)
             
             index_top_int <- tail(index_sel_int, max_num)
             index_0_int <- setdiff(index_sel_int, index_top_int)
             
-            #df_xy[int_index[index_top_int, "index"], "group_id"] <- rep(group_id, length(index_top_int))
-            #df_xy[int_index[index_mimus1_int, "index"], "group_id"] <- rep(0, length(index_mimus1_int))
-            
+
             group_ids[index_logi[index_top_int]] <- rep(group_id, length(index_top_int))
             group_ids[index_logi[index_0_int]] <- rep(0, length(index_0_int))
             
             
         } else {
-            #df_xy[group_id_zero[logi_id], "group_id"] <- rep(group_id, sum(logi_id))
             group_ids[group_id_zero[logi_id]] <- rep(group_id, sum(logi_id))
         } 
         
@@ -202,10 +190,8 @@ group_mz_int <- function(x, y, max_num, tolerance = 0, ppm = 0) {
         
     }
     
-    #res <- df_xy$group_id
     res <- group_ids
-    #res[idx] <- res
-    
+
     res
 }
 
